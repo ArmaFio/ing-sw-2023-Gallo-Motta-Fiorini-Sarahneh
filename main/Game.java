4341325a6c00d8b8609696b4340768ecb3c6bbd7
Matteo <matteo4.gallo@mail.polimi.it>
2023-02-21 19:58:22 +0100

    First commit

---
+++ main/Game.java
@@ -0,0 +1,12 @@
+package main;
+
+public class Game {
+    public Game(){
+        System.out.println(this);
+    }
+
+    @Override
+    public String toString() {
+        return "My Shelfie";
+    }
+}
