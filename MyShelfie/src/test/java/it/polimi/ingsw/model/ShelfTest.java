3671690f2ca2b3d0366112cf64fbbabb40bac68f
Armando <armifio22@gmail.com>
2023-04-04 00:05:32 +0200

    .idea folder deleted

--- MyShelfie/src/test/java/it/polimi/ingsw/model/ShelfTest.java
+++ MyShelfie/src/test/java/it/polimi/ingsw/model/ShelfTest.java
@@ -7,7 +7,8 @@ import org.junit.Test;
 import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.Assert.assertTrue;
+
 
 public class ShelfTest {
 
