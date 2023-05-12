44cb9c5e9a541dac28070e1d39c0e7f32eedf3b8
khaled199812 <126579448+khaled199812@users.noreply.github.com>
2023-05-12 16:13:13 +0200

    Added javafx package containing the viewGui with a controller o it's own.

---
+++ MyShelfie/src/main/java/it/polimi/ingsw/javafx/Controller.java
@@ -0,0 +1,16 @@
+package it.polimi.ingsw.javafx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+
+public class Controller {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Javafx");
+    }
+
+}
